f0ebf2e53c455e491897a309119ac59413c70ec4
Uday Kapila <udaykapila@Udays-MacBook.local>
2026-06-30 14:51:53 +0530

    Fix test configuration for production deployment

--- src/test/java/com/sweetlysweet/backend/BackendApplicationTests.java
+++ src/test/java/com/sweetlysweet/backend/BackendApplicationTests.java
@@ -1,13 +1,10 @@
 package com.sweetlysweet.backend;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class BackendApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
