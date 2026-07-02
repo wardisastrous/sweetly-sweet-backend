764e31841851698b0fab165f6dae6a4844235190
Uday Kapila <udaykapila@Udays-MacBook.local>
2026-07-02 10:56:53 +0530

    Fix order user loading

--- src/main/java/com/sweetlysweet/backend/entity/Order.java
+++ src/main/java/com/sweetlysweet/backend/entity/Order.java
@@ -13,7 +13,7 @@ public class Order {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
